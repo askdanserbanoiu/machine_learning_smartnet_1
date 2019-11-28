e93997ca85547247d76fad7399e53883dc64492e
Dan Serbanoiu <ask@danserbanoiu.com>
2019-11-28 10:31:00 +0200

    added procedure
    added procedure

--- exercise14.py
+++ exercise14.py
@@ -2,10 +2,15 @@ import numpy
 import matplotlib 
 import random
 
+def bayesian_inference(X, y, theta_0, sigma2_n, sigma2_0):
+    XX_transpose = numpy.dot(numpy.transpose(X), X)
+    mu_a = numpy.linalg.inv(numpy.dot(1/sigma2_0, numpy.identity(XX_transpose.__len__())) + numpy.dot(1/sigma2_n, XX_transpose))
+    mu_b = numpy.dot(numpy.linalg.inv(X), y - numpy.dot(X, theta_0))
+    mu = theta_0 + numpy.dot(numpy.dot(1/sigma2_n, mu_a), mu_b)
 
 
 def exercise14(N, test, mu, sigma_square, theta_0):
-    return "@"
+    
    
 
 
