827bf28abd65a2e9448ade03060b88c4cb3f62bd
Dan Serbanoiu <32792904+askdanserbanoiu@users.noreply.github.com>
2019-12-01 14:18:03 +0200

    1_2 data to float
    1_2 data to float

--- exercise2_1.py
+++ exercise2_1.py
@@ -2,8 +2,10 @@ import numpy
 import matplotlib.pyplot as plt 
 import random
 import os
+import re
 
 def read_data():
+    
     data_path = os.path.join(os.path.join(os.getcwd(), "dataex1"))
     
     f1 = open(os.path.join(data_path, "iris.data"), "r")
@@ -13,10 +15,21 @@ def read_data():
     
     if f1.mode == 'r':
         f1_matrix = [[var.strip() for var in item.split(',')] for item in f1.readlines()] 
-        print(f1_matrix)
     if f2.mode == 'r':
         f2_matrix = [[var.strip() for var in item.split(',')] for item in f2.readlines()] 
-        print(f2_matrix)
+        
+    for i in range(0, f1_matrix.__len__()):
+        for j in range(0, f1_matrix[i].__len__()):
+            if re.match(r'^-?\d+(?:\.\d+)?$', f1_matrix[i][j]):
+                f1_matrix[i][j] = float(f1_matrix[i][j])
+                
+    for i in range(0, f2_matrix.__len__()):
+        for j in range(0, f2_matrix[i].__len__()):
+            if re.match(r'^-?\d+(?:\.\d+)?$', f2_matrix[i][j]):
+                f2_matrix[i][j] = float(f2_matrix[i][j])
+                
+    print(f1_matrix)
+    print(f2_matrix)
 
     return [f1_matrix, f2_matrix]
 
