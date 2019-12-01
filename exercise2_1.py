9196f0b0f29fa1b88d4c26db8af4e8af631d049f
Dan Serbanoiu <32792904+askdanserbanoiu@users.noreply.github.com>
2019-12-01 14:00:18 +0200

    up
    up

--- exercise2_1.py
+++ exercise2_1.py
@@ -8,17 +8,17 @@ def read_data():
     
     f1 = open(os.path.join(data_path, "iris.data"), "r")
     f2 = open(os.path.join(data_path, "pima-indians-diabetes.data"), "r")
-    f1l = []
-    f2l = []
+    f1_matrix = []
+    f2_matrix = []
     
     if f1.mode == 'r':
-        f1l = [[var.strip() for var in item.split(',')] for item in f1.readlines()] 
-        print(f1l)
+        f1_matrix = [[var.strip() for var in item.split(',')] for item in f1.readlines()] 
+        print(f1_matrix)
     if f2.mode == 'r':
-        f2l = [[var.strip() for var in item.split(',')] for item in f2.readlines()] 
-        print(f2l)
+        f2_matrix = [[var.strip() for var in item.split(',')] for item in f2.readlines()] 
+        print(f2_matrix)
 
-    return [f1l, f2l]
+    return [f1_matrix, f2_matrix]
 
 def exercise2_1():
     data = read_data()
