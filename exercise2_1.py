8fe15ec8e3335fd1b42f8061bf4e4532aff504b0
Dan Serbanoiu <32792904+askdanserbanoiu@users.noreply.github.com>
2019-12-01 21:12:48 +0200

    exercise2_1
    exercise2_1

--- exercise2_1.py
+++ exercise2_1.py
@@ -4,6 +4,13 @@ import random
 import os
 import re
 
+
+def column(matrix, i):
+    return [row[i] for row in matrix]
+
+def most_frequent(List): 
+    return max(set(List), key = List.count) 
+
 def read_data():
     
     data_path = os.path.join(os.path.join(os.getcwd(), "exercise2"))
@@ -28,13 +35,59 @@ def read_data():
             if re.match(r'^-?\d+(?:\.\d+)?$', f2_matrix[i][j]):
                 f2_matrix[i][j] = float(f2_matrix[i][j])
                 
-    print(f1_matrix)
-    print(f2_matrix)
-
     return [f1_matrix, f2_matrix]
 
+def k_nearest_neighbor_classifier(k, test_set, training_set):
+    
+    distances = []
+    for i in range(0, training_set.__len__() - 1):
+        distance = 0
+        for j in range(0, training_set[i].__len__() - 2):
+            distance = distance + (training_set[i][j] - test_set[j])**2
+            
+        distances.append([distance, training_set[i][training_set[i].__len__() - 1]])
+    
+    distances.sort()
+    nearest = most_frequent(column(distances[0:k], 1))
+    return nearest
+    
 def exercise2_1():
     data = read_data()
+    
+    iris = data[0]
+    prima_indians_diabetes = data[1]
+    
+    right_guesses_iris = 0
+    wrong_guesses_iris = 0
+        
+    for i in range(0, iris.__len__() - 1):
+        result = k_nearest_neighbor_classifier(3, iris[i], [x for j, x in enumerate(iris) if j != i])
+        if (result == iris[i][iris[i].__len__() - 1]):
+            right_guesses_iris = right_guesses_iris + 1
+        else:
+            wrong_guesses_iris = wrong_guesses_iris + 1
+            
+    frequency_right_iris = (right_guesses_iris/iris.__len__())*100
+    frequency_wrong_iris = (wrong_guesses_iris/iris.__len__())*100
+    
+    print(frequency_right_iris)
+    print(frequency_wrong_iris)
+    
+    right_guesses_indians = 0
+    wrong_guesses_indians = 0
+        
+    for i in range(0, prima_indians_diabetes.__len__() - 1):
+        result = k_nearest_neighbor_classifier(3, prima_indians_diabetes[i], [x for j, x in enumerate(prima_indians_diabetes) if j != i])
+        if (result == prima_indians_diabetes[i][prima_indians_diabetes[i].__len__() - 1]):
+            right_guesses_indians = right_guesses_indians + 1
+        else:
+            wrong_guesses_indians = wrong_guesses_indians + 1
+            
+    frequency_right_indians = (right_guesses_indians/prima_indians_diabetes.__len__())*100
+    frequency_wrong_indians = (wrong_guesses_indians/prima_indians_diabetes.__len__())*100
+    
+    print(frequency_right_indians)
+    print(frequency_wrong_indians)
 
     return
    
